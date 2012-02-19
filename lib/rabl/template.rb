31843eda03218a7ad8fef39ec4a722ae278d71ae
Muhammad Syaiful Sabril <syaiful.sabril@gmail.com>
2012-02-20 00:33:58 +0700

    check rails 3

--- lib/rabl/template.rb
+++ lib/rabl/template.rb
@@ -41,7 +41,7 @@ if defined?(ActionView) && defined?(Rails) && Rails.version =~ /^2/
 end
 
 # Rails 3.X Template
-if defined?(ActionView) && defined?(Rails) && Rails.version =~ /^3/
+if defined?(ActionView) && defined?(Rails) && (Rails.version =~ /^3/) == 0
   puts "RAILS 3"
   module ActionView
     module Template::Handlers
